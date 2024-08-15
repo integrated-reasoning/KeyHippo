dc51ffc6cc901582bb500d191b34d932e6fa71d3
David Cox <david@integrated-reasoning.com>
2024-08-15 12:19:45 -0700

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Issue API keys for your Supabase app with one line of SQL";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+        buildInputs = with pkgs; [
+          nodejs_20
+          nodePackages.pnpm
+          nodePackages.ts-node
+          openssl
+          openssl.dev
+        ];
+      in
+      rec
+      {
+        devShell = pkgs.mkShell {
+          inherit buildInputs;
+        };
+      }
+    );
+}
